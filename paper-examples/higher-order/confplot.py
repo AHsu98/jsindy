631e7321949055e062c7117d781e0bc9419dc30e
Jake Stevens-Haas <37048747+Jacob-Stevens-Haas@users.noreply.github.com>
2025-08-29 12:47:19 -0700

    Built VDP plot for paper

---
+++ paper-examples/higher-order/confplot.py
@@ -0,0 +1,34 @@
+SIM_KW = {
+    "color": "purple",
+    'alpha': 0.4,
+    "lw": 5,
+    "linestyle": "-",
+    "label": "Simulated Dynamics",
+}
+EST_KW = {
+    "color": "blue",
+    "lw": 5,
+    "alpha": 0.4,
+    "linestyle": "-",
+    "label": "State Estimation",
+    "zorder": 1
+}
+TRUE_KW = {
+    "color": "black",
+    "linestyle": "-",
+    "label": "True Trajectory",
+    "zorder": 2
+}
+TRUE_UNS_KW = {
+    "color": "black",
+    "linestyle": "--",
+    "label": "True Unseen Trajectory",
+}
+OBS_KW = {
+    "facecolors":"red",
+    "edgecolors": "black",
+    "lw":1,
+    "s": 20,
+    "label": "Observations",
+    "zorder": 3
+}
