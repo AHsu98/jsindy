47812e49fa5c3ebd7d5938757c8a63fe7a4767bc
Alex Hsu <owlx@uw.edu>
2026-02-16 16:42:12 -0800

    Removed pysindy-experiments dependency

--- exp/evaluate/jsindy.py
+++ exp/evaluate/jsindy.py
@@ -8,7 +8,7 @@ from jsindy.trajectory_model import DataAdaptedRKHSInterpolant
 from jsindy.dynamics_model import FeatureLinearModel
 from jsindy.optim import AlternatingActiveSetLMSolver, LMSettings
 
-from exp.expdata import ExpData, GenExAdapter
+from exp.expdata import ExpData
 from exp.metrics import coeff_metrics, data_metrics
 import pickle
 import jax.numpy as jnp
@@ -53,40 +53,3 @@ def evaluate_jmodel(model:JSINDyModel, expdata:ExpData,  save_path: str = None):
             pickle.dump(metrics, f)
     
     return metrics
-
-
-def evaluate_jmodel_alt(model:JSINDyModel, expdata:GenExAdapter,  save_path: str = None):
-
-    # proxy_model for compatability with regular sindy functions
-    # Definitely not type safe, can only use proxy_model with limited functions
-    proxy_model = ps.SINDy(feature_library = model.dynamics_model.feature_map)
-    proxy_model =  ps.SINDy(feature_library = model.dynamics_model.feature_map)
-    proxy_model.model = True
-    proxy_model.feature_names = expdata.feature_names
-    proxy_model.coefficients = lambda: model.theta.T
-
-    coeff_true, coeff_est, names = unionize_coeff_matrices(proxy_model, expdata.true_coeff)
-    metrics = {}
-    
-    # metrics["theta"] = model.theta
-
-    # states prediction 
-    metrics["x_metrics"] = data_metrics(
-        pred_sim = model.predict_state(expdata.t_true),
-        true = expdata.x_true
-    )
-
-    # save learned coeffs
-    metrics["theta_metrics"]  = legacy_coeff_metrics(coeff_est, coeff_true)
-
-    metrics["xdot_metrics"] = data_metrics(
-        pred_sim = model.predict(expdata.x_true),
-        true = expdata.x_dot
-    )
-
-    if save_path:
-        with open(save_path, 'wb') as f:
-            pickle.dump(metrics, f)
-    
-    return metrics
-
